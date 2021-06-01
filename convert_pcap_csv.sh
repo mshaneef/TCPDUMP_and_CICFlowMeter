8afbbfda5450cf3e3535451460b5f8f1508e162a
mshaneef <20142409@stu.uob.edu.bh>
2021-06-01 12:25:59 +0300

    fixed the code to run on our machine

--- convert_pcap_csv.sh
+++ convert_pcap_csv.sh
@@ -6,7 +6,7 @@ pcap_file="$1"
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"  # On the same directory.
 echo ">>> Script dir: ${script_dir}"
 
-output_dir="${script_dir}"/csv
+output_dir="$2"
 
 
 ## Clean
@@ -17,16 +17,6 @@ cancel() {
 	exit 0
 }
 trap 'cancel' INT TERM
-
-cleanup() {
-#    echo "+++ Remove ${pcap_file}"
-#    rm -f "${pcap_file}"
-
-	echo "+++ Finish the conversion"
-	echo
-    exit 0
-}
-trap 'cleanup' EXIT
 
 
 ## Convert
@@ -39,39 +29,3 @@ echo "    Output dir: ${output_dir}"
 cic="${script_dir}"/CICFlowMeters/CICFlowMeter-4.0/bin/CICFlowMeter
 
 "${cic}" "${pcap_file}" "${output_dir}"
-
-
-echo "+++ Remove ${pcap_file}"
-rm -f "${pcap_file}"
-
-
-
-
-## Rearrange the PCAP due to other CICFlowMeter versions' compatibility
-# pcap_dir="${script_dir}"/tmp
-# pcap_tmp="${pcap_dir}"/$(basename "${pcap_file}")
-
-#echo "> move ${pcap_file} to ${pcap_tmp}"
-#mv "${pcap_file}" "${pcap_dir}"
-
-
-# CICFlowMeterV2
-#cic_dir="${script_dir}"/CICFlowMeters/CICFlowMeterV2
-#java -Djava.library.path="${cic_dir}" -jar "${cic_dir}"/CICFlowMeter.jar "${pcap_dir}/" "${output_dir}/2/"
-
-#cic_dir=${HOME}/CICFlowMeterV2
-#java -Djava.library.path="${cic_dir}/jnetpcap" -jar "${cic_dir}"/CICFlowMeter.jar "${pcap_dir}/" "${output_dir}/2/"
-
-# CICFlowMeterV3-jnetpcap-1.3
-#cic_dir="${script_dir}"/CICFlowMeters/CICFlowMeterV3-jnetpcap-1.3
-#java -Djava.library.path="${cic_dir}" -jar "${cic_dir}"/CICFlowMeterV3.jar "${pcap_tmp}" "${output_dir}/3.1.3/"
-
-# CICFlowMeterV3-jnetpcap-1.4
-#cic_dir="${script_dir}"/CICFlowMeters/CICFlowMeterV3-jnetpcap-1.4
-#java -Djava.library.path="${cic_dir}" -jar "${cic_dir}"/CICFlowMeterV3.jar "${pcap_tmp}" "${output_dir}/3.1.4/"
-
-
-#echo "+++ remove ${pcap_tmp}"
-#rm -f "${pcap_tmp}"
-
-
